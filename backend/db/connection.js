38901f4362842f42b9a9dba0c016dde53b357402
stuffsergio <sergioperezmontalvo@gmail.com>
2026-06-29 09:18:06 +0200

    fix prod

--- backend/db/connection.js
+++ backend/db/connection.js
@@ -3,6 +3,7 @@ import mysql from "mysql2/promise";
 export const pool = await mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
+  port: process.env.DB_PORT || 3306,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   waitForConnections: true,
